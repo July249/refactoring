0ea5e70b97ad4af08140c494a8075f9c448a57d4
jihyunglee <jhlee7125@gmail.com>
2023-01-09 19:45:16 +0900

    23-01-09 / 6-2 함수 인라인하기

--- ch06/6-2.js
+++ ch06/6-2.js
@@ -1,20 +1,17 @@
-// 예제 1
-export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-}
+// 함수 인라인하기
 
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
+// 예제 1
+// moreThanFiveLateDeliveries의 재사용성이 높지 않고, 값의 변화가 빈번하게 일어나지 않는다면
+// 굳이 함수로 추출하여 사용할 이유는 없다.
+export function rating(driver) {
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 // 예제 2
+// 불필요한 간접 호출은 지양하는 것이 좋다.
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
-}
-
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
+  const result = [];
+  result.push(["name", customer.name]);
+  result.push(["location", customer.location]);
+  return result;
 }
