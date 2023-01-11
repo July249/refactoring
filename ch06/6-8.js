60af16450f3da876bf21cd2a14b74e20bb03bd6b
jihyunglee <jhlee7125@gmail.com>
2023-01-11 16:42:00 +0900

    23-01-11 / 6-8 매개변수 객체 만들기 - 2

--- ch06/6-8.js
+++ ch06/6-8.js
@@ -1,11 +1,32 @@
 // 매개변수 객체 만들기
 export function readingsOutsideRange(station, range) {
-  return station.readings.filter(
-    (readings) =>
-      readings.temp < range.temperatureFloor ||
-      readings.temp > range.temperatureCeiling
-  );
+  return station.readings.filter((readings) => !range.contain(readings.temp));
 }
+
+// 순수 데이터 객체를 수정
+// range 범위 안에 있는지 판단하는 함수를 만들어 관리
+export class NumberRange {
+  #min;
+  #max;
+  constructor(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+
+  get min() {
+    return this.#min;
+  }
+
+  get max() {
+    return this.#max;
+  }
+
+  contains(number) {
+    return number >= this.#min && number <= this.#max;
+  }
+}
+
+const operationPlan = new NumberRange(51, 53);
 
 const station = {
   name: "ZB1",
@@ -17,13 +38,5 @@ const station = {
     { temp: 51, time: "2016-11-10 09:50" },
   ],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+readingsOutsideRange(station, operationPlan);
