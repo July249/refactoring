e21888a9b5c4b97d8672f2a2caae6998c562fed8
jihyunglee <jhlee7125@gmail.com>
2023-01-11 13:32:30 +0900

    23-01-11 / 6-5 함수 선언 바꾸기

--- ch06/6-5-1.js
+++ ch06/6-5-1.js
@@ -1,3 +1,5 @@
-export function circum(radius) {
+// 함수 선언 바꾸기
+// Tip: 변수, 함수 이름 우클릭 => Rename Symbol => 해당 이름을 사용하는 모든 것들의 이름을 변경시켜줌
+export function circumference(radius) {
   return 2 * Math.PI * radius;
 }
