00f96351a61bc50642d4c31e337ce96092ed1bd2
jihyunglee <jhlee7125@gmail.com>
2023-01-11 16:52:17 +0900

    23-01-11 / 6-6 변수 캡슐화하기 - 클래스를 이용한 객체 불변성 보장

--- ch06/6-6.js
+++ ch06/6-6.js
@@ -1,6 +1,25 @@
 // 변수 캡슐화하기
-let defaultOwner = { firstName: "마틴", lastName: "파울러" };
+// 깊은 복사를 할 수 있도록 class로 설정
+
+class Person {
+  #firstName;
+  #lastName;
+  constructor(data) {
+    this.#firstName = data.firstName;
+    this.#lastName = data.lastName;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+}
+
+let defaultOwner = new Person({ firstName: "마틴", lastName: "파울러" });
 
 export function getDefaultOwner() {
-  return { ...defaultOwner };
+  return defaultOwner;
 }
