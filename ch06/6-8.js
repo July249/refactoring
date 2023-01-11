a34ead62083ff813eda8d04b976ea37159cd2e34
jihyunglee <jhlee7125@gmail.com>
2023-01-11 16:31:57 +0900

    23-01-11 / 6-8 매개변수 객체 만들기 - 1

--- ch06/6-8.js
+++ ch06/6-8.js
@@ -1,15 +1,20 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+// 매개변수 객체 만들기
+export function readingsOutsideRange(station, range) {
+  return station.readings.filter(
+    (readings) =>
+      readings.temp < range.temperatureFloor ||
+      readings.temp > range.temperatureCeiling
+  );
 }
 
 const station = {
-  name: 'ZB1',
+  name: "ZB1",
   readings: [
-    { temp: 47, time: '2016-11-10 09:10' },
-    { temp: 53, time: '2016-11-10 09:20' },
-    { temp: 58, time: '2016-11-10 09:30' },
-    { temp: 53, time: '2016-11-10 09:40' },
-    { temp: 51, time: '2016-11-10 09:50' },
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
   ],
 };
 const operationPlan = {
