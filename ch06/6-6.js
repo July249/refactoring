a54f39f525d1ae4e803ee850075bc08d4a338881
jihyunglee <jhlee7125@gmail.com>
2023-01-11 16:47:47 +0900

    23-01-11 / 6-6 변수 캡슐화하기 - 객체 불변성

--- ch06/6-6.js
+++ ch06/6-6.js
@@ -1,5 +1,6 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+// 변수 캡슐화하기
+let defaultOwner = { firstName: "마틴", lastName: "파울러" };
 
 export function getDefaultOwner() {
-  return defaultOwner;
+  return { ...defaultOwner };
 }
