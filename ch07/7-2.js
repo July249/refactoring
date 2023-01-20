7a1a443882301af5bcc82a6aa7a4f29597ce7989
jihyunglee <jhlee7125@gmail.com>
2023-01-20 23:47:30 +0900

    23-01-20 / 7-2 컬렉션 캡슐화하기

--- ch07/7-2.js
+++ ch07/7-2.js
@@ -1,3 +1,7 @@
+// 컬렉션 캡슐화하기
+
+// 외부에 컬렉션을 노출하여 변경가능하게 하지 말고
+// 어떤 동작이 되는지 api를 만들어서 해당 api를 통해서만 접근이 가능하도록 하는 것이 중요
 export class Person {
   #name;
   #courses;
@@ -11,12 +15,27 @@ export class Person {
   }
 
   get courses() {
-    return this.#courses;
+    return [...this.#courses];
+    // return this.#courses;
   }
 
-  set courses(courses) {
-    this.#courses = courses;
+  addCourse(course) {
+    this.#courses.push(course);
   }
+
+  removeCourse(course, runIfAbsent) {
+    const index = this.#courses.indexOf(course);
+    if (index === -1) {
+      runIfAbsent();
+      return;
+    }
+    this.#courses.splice(index, 1);
+  }
+
+  // 외부에서 업데이트를 할 수 없게 만들 것임
+  // set courses(courses) {
+  //   this.#courses = courses;
+  // }
 }
 
 export class Course {
@@ -36,6 +55,15 @@ export class Course {
   }
 }
 
-const ellie = new Person('엘리');
-ellie.courses.push(new Course('리팩토링', true));
+const ellie = new Person("엘리");
+const course = new Course("리팩토링", true);
+ellie.addCourse(course);
 console.log(ellie.courses.length);
+ellie.removeCourse(course, () => {
+  console.log("해당 코스는 없습니다.");
+});
+console.log(ellie.courses);
+console.log(ellie.courses.length);
+ellie.removeCourse(course, () => {
+  console.log("해당 코스는 없습니다.");
+});
