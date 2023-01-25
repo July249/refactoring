264536d877ca9e2dc3c08cbdfb0b12fd8d52ce17
jihyunglee <jhlee7125@gmail.com>
2023-01-25 13:31:29 +0900

    23-01-25 / 8-2 필드 옮기기

--- ch08/8-2-2.js
+++ ch08/8-2-2.js
@@ -1,17 +1,30 @@
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
-    this._interestRate = interestRate;
   }
 
+  get interestRate() {
+    return this._type.interestRate;
+  }
+}
+
+class AccountType {
+  constructor(nameString, interestRate) {
+    this._name = nameString;
+    this._interestRate = interestRate;
+  }
   get interestRate() {
     return this._interestRate;
   }
 }
 
-class AccountType {
-  constructor(nameString) {
-    this._name = nameString;
-  }
+const fixedDeposit = new AccountType("정기예금", 0.05);
+const myAccount = new Account("123-9876-65-0123", fixedDeposit);
+console.log(myAccount);
+/* 
+Account {
+  _number: '123-9876-65-0123',
+  _type: AccountType { _name: '정기예금', _interestRate: 0.05 }
 }
+*/
