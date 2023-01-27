a03664592c05092c74cb3e316a8800c1f70750bf
jihyunglee <jhlee7125@gmail.com>
2023-01-27 16:48:09 +0900

    23-01-27 / 9-5 값을 참조로 바꾸기

--- ch09/9-5.js
+++ ch09/9-5.js
@@ -1,7 +1,21 @@
+const customerRepoistory = new CustomerRepoistory();
+
+const order = new Order(
+  data.number,
+  customerRepoistory.registerCustomer(data.customerId)
+);
+
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  // constructor(data) {
+  //   this._number = data.number;
+  //   // this._customer = new Customer(data.customerId);
+  //   // 새로운 customer를 계속 만드는 것이 아니라!
+  //   // this._customer = customerRepoistory.registerCustomer(data.customerId);
+  //   // => 조금 헤비함
+  // }
+  constructor(number, customer) {
+    this._number = number;
+    this._customer = customer;
   }
 
   get customer() {
@@ -10,11 +24,31 @@ class Order {
 }
 
 class Customer {
-  constructor(id) {
+  constructor(id, name) {
     this._id = id;
+    this._name = name;
   }
 
   get id() {
     return this._id;
+  }
+}
+
+// customerId가 유일하다는 것을 보장하기 위함
+class CustomerRepoistory {
+  #customers;
+  constructor() {
+    this.#customers = new Map();
+  }
+
+  registerCustomer(id) {
+    if (!this.#customer.has(id)) {
+      this.#customers.set(id, new Customer(id));
+    }
+    return findCustomer(id);
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id);
   }
 }
