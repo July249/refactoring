59df425528b08ccdfe1aa3cfbfb01830f9e4b489
jihyunglee <jhlee7125@gmail.com>
2023-01-27 10:49:38 +0900

    23-01-27 / 10-1 과제 피드백

--- ch10/10-1.js
+++ ch10/10-1.js
@@ -1,7 +1,15 @@
 function calculateCharge(date, quantity, plan) {
-  const summerCharge = quantity * plan.summerRate;
-  const regularCharge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd)
-    ? summerCharge
-    : regularCharge;
+  return isSummer() ? summerCharge() : regularCharge();
+
+  function isSummer() {
+    return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+  }
+
+  function summerCharge() {
+    return quantity * plan.summerRate;
+  }
+
+  function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+  }
 }
