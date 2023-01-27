5d0d7d79563866592abe416b416157aad61f1abc
jihyunglee <jhlee7125@gmail.com>
2023-01-27 10:40:26 +0900

    23-01-27 / 10-1 과제 구현

--- ch10/10-1.js
+++ ch10/10-1.js
@@ -1,7 +1,7 @@
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+  const summerCharge = quantity * plan.summerRate;
+  const regularCharge = quantity * plan.regularRate + plan.regularServiceCharge;
+  return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd)
+    ? summerCharge
+    : regularCharge;
 }
