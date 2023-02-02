d29c212f4473f40f88b0ed4b707d183b5fe11beb
이지형 <jihyeonglee@ijihyeong-ui-noteubug.local>
2023-02-02 09:12:27 +0900

    23/02/01 / 10-6 어서션 추가하기

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,15 @@
+// 어서션 추가하기
+// - 절대로 실수하면 안되는 경우에 사용한다.
+import { strict as assert } from "node:assert";
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
+    assert(number >= 0);
     return this.discountRate ? number - this.discountRate * number : number;
   }
 }
+
+new Customer().applyDiscount(1);
