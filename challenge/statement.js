1eb46703e24b6717b7a2a67eac1f7c1c884911ef
jihyunglee <jhlee7125@gmail.com>
2023-02-17 07:55:57 +0900

    13-7 데이터 생성 로직인 createStatement 분리

--- challenge/statement.js
+++ challenge/statement.js
@@ -1,63 +1,8 @@
+import { createStatement } from "./createStatement";
+
 export function statement(invoice, plays) {
-  // 출력을 위해 필요한 모든 데이터가 들어있는 statement 객체
-  const statement = {};
-  statement.customer = invoice.customer;
-  // statement.performances = invoice.performances.map((p) => enrichPerformance(p));
-  statement.performances = invoice.performances.map(enrichPerformance);
-  statement.totalAmount = totalAmount(statement.performances);
-  statement.totalCredits = totalCredits(statement.performances);
+  const statement = createStatement(invoice, plays);
   return renderPlainText(statement);
-
-  function enrichPerformance(performance) {
-    const result = { ...performance };
-    result.play = playFor(performance);
-    result.amount = amountFor(result);
-    result.credits = creditsFor(result);
-    return result;
-  }
-
-  function playFor(performance) {
-    return plays[performance.playID];
-  }
-
-  function amountFor(performance) {
-    let thisAmount = 0;
-    switch (performance.play.type) {
-      case "tragedy": // 비극
-        thisAmount = 40000;
-        if (performance.audience > 30) {
-          thisAmount += 1000 * (performance.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        thisAmount = 30000;
-        if (performance.audience > 20) {
-          thisAmount += 10000 + 500 * (performance.audience - 20);
-        }
-        thisAmount += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-    }
-    return thisAmount;
-  }
-
-  function creditsFor(performance) {
-    let result = 0;
-    result += Math.max(performance.audience - 30, 0);
-    if ("comedy" === performance.play.type) {
-      result += Math.floor(performance.audience / 5);
-    }
-    return result;
-  }
-
-  function totalAmount(performances) {
-    return performances.reduce((sum, p) => (sum += p.amount), 0);
-  }
-
-  function totalCredits(performances) {
-    return performances.reduce((sum, p) => (sum += p.credits), 0);
-  }
 }
 
 function renderPlainText(statement) {
