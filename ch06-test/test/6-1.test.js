569848ed60e286d8e8a5226fb512f5bf47aff1e3
jihyunglee <jhlee7125@gmail.com>
2023-02-18 16:41:58 +0900

    14-4. Clock 의존성 주입

--- ch06-test/test/6-1.test.js
+++ ch06-test/test/6-1.test.js
@@ -10,8 +10,25 @@ class Console {
     return this.#content;
   }
 }
+
+class Clock {
+  constructor() {}
+  get today() {
+    return {
+      getFullYear() {
+        return 2022;
+      },
+      getMonth() {
+        return 0;
+      },
+      getDate() {
+        return 21;
+      },
+    };
+  }
+}
 describe("printOwing", () => {
-  it("Should print owing", () => {
+  it("should print owing", () => {
     const invoice = {
       orders: [{ amount: 2 }, { amount: 5 }],
       customer: "엘리",
@@ -22,9 +39,10 @@ describe("printOwing", () => {
       "***********************\n" +
       "name: 엘리\n" +
       "amount: 7\n" +
-      "due: 2/20/2022\n";
+      "due: 2022. 2. 20.\n";
     const console = new Console();
-    printOwing(invoice, console);
+    const clock = new Clock();
+    printOwing(invoice, console, clock);
     expect(console.content).toBe(expected);
   });
 });
